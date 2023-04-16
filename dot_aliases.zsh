ded29baa49acdf1997c3e03549740ca2ce4ce46b
Juan Barajas <jbperches@gmail.com>
2023-04-15 19:30:47 -0600

    feat: add zsh config

---
+++ dot_aliases.zsh
@@ -0,0 +1,38 @@
+# vanilla
+alias cl="clear"
+alias cls="cl; ls"
+alias cppwd="pwd && pwd | xclip"
+alias openapp="xdg-open"
+alias watch="watch " # allow for watching aliases
+alias xclip="xclip -selection c -rmlastnl"
+
+# git
+alias g="git"
+alias wkt="git-fzf worktree list"
+
+# neovim
+alias nv="nvim"
+alias nvide="neovide --multigrid"
+alias nvs="sudo -E -s nvim"
+
+# python
+alias ipy="ipython"
+alias jl="jupyter-lab"
+alias pt="poetry"
+
+# viztracer
+alias vtrace="viztracer --output_file ./profile.json --min_duration 25 --max_stack_depth 25 --"
+alias vview="vizviewer"
+
+# lazydocker
+alias lzd="TERM=xterm-kitty lazydocker"
+
+# kitty
+alias icat="kitty +kitten icat"
+alias s="kitty +kitten ssh"
+
+# docker
+alias compose-run="docker compose run --rm"
+
+# kubernetes
+alias mkctl="microk8s kubectl"

f8291ab8a52ea256f3cc4ef2ade52ac2267d70c0
Juan Barajas <jbperches@gmail.com>
2023-06-27 13:19:33 -0600

    feat(nvim): add custom sorting to cmp

--- private_dot_config/nvim/lua/plugins/editing.lua
+++ private_dot_config/nvim/lua/plugins/editing.lua
@@ -100,7 +100,6 @@ return {
       vim.o.completeopt = "menu,menuone,noselect"
 
       local cmp = require("cmp")
-      local cmp_buffer = require('cmp_buffer')
 
       local kind_icons = {
         Text = "",
@@ -186,7 +185,7 @@ return {
 
         sources = {
           { name = "nvim_lua" },
-          { name = "nvim_lsp" },
+          { name = "nvim_lsp", keyword_length = 3 },
           { name = "luasnip" },
           { name = "path" },
           {
@@ -201,8 +200,30 @@ return {
 
         sorting = {
           comparators = {
-            function(...) return cmp_buffer:compare_locality(...) end,
-          }
+            cmp.config.compare.offset,
+            cmp.config.compare.exact,
+            cmp.config.compare.score,
+
+            cmp.config.compare.recently_used,
+
+            -- from cmp-under
+            function(entry1, entry2)
+              local _, entry1_under = entry1.completion_item.label:find "^_+"
+              local _, entry2_under = entry2.completion_item.label:find "^_+"
+              entry1_under = entry1_under or 0
+              entry2_under = entry2_under or 0
+              if entry1_under > entry2_under then
+                return false
+              elseif entry1_under < entry2_under then
+                return true
+              end
+            end,
+
+            cmp.config.compare.kind,
+            cmp.config.compare.sort_text,
+            cmp.config.compare.length,
+            cmp.config.compare.order,
+          },
         },
 
         formatting = {

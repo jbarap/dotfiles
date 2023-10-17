4645ebfaa2881fc5f9e609cb0e193160b8713e8f
Juan Barajas <jbperches@gmail.com>
2023-10-17 00:40:15 -0600

    feat(nvim): use oil instead of neo-tree

--- private_dot_config/nvim/lua/plugins/tools.lua
+++ private_dot_config/nvim/lua/plugins/tools.lua
@@ -1,175 +1,59 @@
 return {
-  -- File tree
+  -- File explorer and modifier
   {
-    "nvim-neo-tree/neo-tree.nvim",
-    cmd = "Neotree",
-    branch = "v3.x",
+    "stevearc/oil.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    cmd = "Oil",
     keys = {
-      { "<Leader>nn", "<cmd>Neotree filesystem focus left toggle<CR>", desc =  "Neotree toggle"  },
-      { "<Leader>nf", "<cmd>Neotree filesystem focus reveal toggle<CR>", desc =  "Neotree toggle (focused on file)"  },
-      { "<Leader>ng", "<cmd>Neotree git_status left<CR>", desc =  "Neotree git status"  },
+      { "-", function() require("oil").open() end, desc = "Open buffer's directory" },
     },
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons",
-      "MunifTanjim/nui.nvim",
-    },
-    init = function ()
-      -- load neo-tree on directory open so it hijacks netrw
-      vim.cmd("silent! autocmd! FileExplorer *")
-      vim.api.nvim_create_autocmd(
-        { "BufEnter", "BufWinEnter" },
-        {
-          pattern = { "*" },
-          callback = function ()
-            local bufname = vim.api.nvim_buf_get_name(0)
-            local stats = vim.loop.fs_stat(bufname)
-            if not stats then
-              return false
+    -- lazy setup: https://github.com/folke/lazy.nvim/issues/533#issuecomment-1489174249
+    init = function()
+      if vim.fn.argc() == 1 then
+        local stat = vim.loop.fs_stat(vim.fn.argv(0))
+        -- Capture the protocol and lazy load oil if it is "oil-ssh", besides also lazy
+        -- loading it when the first argument is a directory.
+        local adapter = string.match(vim.fn.argv(0), "^([%l-]*)://")
+        if (stat and stat.type == "directory") or adapter == "oil-ssh" then
+          require("lazy").load({ plugins = { "oil.nvim" } })
+        end
+      end
+      if not require("lazy.core.config").plugins["oil.nvim"]._.loaded then
+        vim.api.nvim_create_autocmd("BufNew", {
+          callback = function()
+            if vim.fn.isdirectory(vim.fn.expand("<afile>")) == 1 then
+              require("lazy").load({ plugins = { "oil.nvim" } })
+              -- Once oil is loaded, we can delete this autocmd
+              return true
             end
-            if stats.type ~= "directory" then
-              return false
-            end
-            require("neo-tree")
-          end
-        }
-      )
+          end,
+        })
+      end
     end,
     opts = {
-      close_if_last_window = false,
-      popup_border_style = "rounded",
-      enable_git_status = true,
-      enable_diagnostics = false,
-      default_component_configs = {
-        indent = {
-          indent_size = 2,
-          padding = 1, -- extra padding on left hand side
-          -- indent guides
-          with_markers = true,
-          indent_marker = "│",
-          last_indent_marker = "└",
-          highlight = "NeoTreeIndentMarker",
-          -- expander config, needed for nesting files
-          with_expanders = nil, -- if nil and file nesting is enabled, will enable expanders
-          expander_collapsed = "",
-          expander_expanded = "",
-          expander_highlight = "NeoTreeExpander",
-        },
-        icon = {
-          folder_closed = "",
-          folder_open = "",
-          folder_empty = "ﰊ",
-          default = "*",
-        },
-        name = {
-          trailing_slash = false,
-          use_git_status_colors = false,
-        },
-        git_status = {
-          symbols = {
-            -- Change type
-            added = "✚",
-            deleted = "✖",
-            modified = "",
-            renamed = "",
-            -- Status type
-            untracked = "",
-            ignored = "",
-            unstaged = "",
-            staged = "",
-            conflict = "",
-          },
-        },
+      columns = {
+        "icon",
       },
-      window = {
-        position = "left",
-        width = 40,
-        mappings = {
-          ["<space>"] = "",
-          ["<2-LeftMouse>"] = "open",
-          ["<cr>"] = "open",
-          ["s"] = "open_split",
-          ["v"] = "open_vsplit",
-          ["C"] = "close_node",
-          ["<bs>"] = "navigate_up",
-          ["."] = "set_root",
-          ["H"] = "toggle_hidden",
-          ["R"] = "refresh",
-          -- ["/"] = "fuzzy_finder",
-          ["/"] = "",
-          ["f"] = "filter_on_submit",
-          ["w"] = "",
-          ["<c-x>"] = "clear_filter",
-          ["a"] = "add",
-          ["A"] = "add_directory",
-          ["d"] = "delete",
-          ["r"] = "rename",
-          ["y"] = function (state) -- copy absolute path to system clipboard
-            local node = state.tree:get_node()
-            vim.fn.setreg("+", node.path)
-            print(string.format("Copied path '%s' to clipboard.", node.path))
-          end,
-          ["Y"] = "copy_to_clipboard", -- copy file to system clipboard
-          ["x"] = "cut_to_clipboard",
-          ["p"] = "paste_from_clipboard",
-          ["c"] = "copy", -- takes text input for destination
-          ["m"] = "move", -- takes text input for destination
-          ["q"] = "close_window",
-        },
+      delete_to_trash = false,
+      keymaps = {
+        ["g?"] = "actions.show_help",
+        ["<CR>"] = "actions.select",
+        ["<C-v>"] = "actions.select_vsplit",
+        ["<C-s>"] = "actions.select_split",
+        ["<C-t>"] = "actions.select_tab",
+        ["<C-p>"] = "actions.preview",
+        ["<C-c>"] = "actions.close",
+        ["<C-r>"] = "actions.refresh",
+        ["-"] = "actions.parent",
+        ["_"] = "actions.open_cwd",
+        ["`"] = "actions.cd",
+        ["~"] = "actions.tcd",
+        ["gs"] = "actions.change_sort",
+        ["gx"] = "actions.open_external",
+        ["g."] = "actions.toggle_hidden",
       },
-      nesting_rules = {},
-      filesystem = {
-        filtered_items = {
-          visible = true, -- when true, they will just be displayed differently than normal items
-          hide_dotfiles = true,
-          hide_gitignored = false,
-          hide_by_name = {
-            ".DS_Store",
-            "thumbs.db",
-          },
-          never_show = { -- remains hidden even if visible is toggled to true
-          },
-        },
-        follow_current_file = {
-          enabled = false,
-        },
-        hijack_netrw_behavior = "open_default",
-        use_libuv_file_watcher = true,
-        --
-        window = {
-          mappings = {
-            ["gu"] = "git_unstage_file",
-            ["ga"] = "git_add_file",
-            ["gx"] = "git_revert_file",
-            ["gc"] = "git_commit",
-            ["gp"] = "git_push",
-            ["gg"] = "git_commit_and_push",
-          },
-        },
-      },
-      buffers = {
-        show_unloaded = true,
-        window = {
-          mappings = {
-            ["bd"] = "buffer_delete",
-          },
-        },
-      },
-      git_status = {
-        window = {
-          position = "float",
-          mappings = {
-            ["A"] = "git_add_all",
-            ["gu"] = "git_unstage_file",
-            ["ga"] = "git_add_file",
-            ["gr"] = "git_revert_file",
-            ["gc"] = "git_commit",
-            ["gp"] = "git_push",
-            ["gg"] = "git_commit_and_push",
-          },
-        },
-      },
-    }
+      use_default_keymaps = false,
+    },
   },
 
   -- Debugging

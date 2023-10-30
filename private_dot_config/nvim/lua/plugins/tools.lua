a62dd1cc831e55efd69b76b5dfe3b52e781249b5
Juan Barajas <jbperches@gmail.com>
2023-10-29 22:46:44 -0600

    feat(nvim): add the perfanno tool

--- private_dot_config/nvim/lua/plugins/tools.lua
+++ private_dot_config/nvim/lua/plugins/tools.lua
@@ -471,4 +471,22 @@ return {
     end,
   },
 
+  -- Perf result visualizer
+  {
+    "t-troebst/perfanno.nvim",
+    cmd = {
+      "PerfLoadCallGraph", "PerfLoadFlameGraph", "PerfLuaProfileStart",
+      "PerfPickEvent", "PerfCycleFormat", "PerfHottestLines"
+    },
+    config = function ()
+      local util = require("perfanno.util")
+      local bgcolor = vim.fn.synIDattr(vim.fn.hlID("Normal"), "bg", "gui")
+
+      require("perfanno").setup({
+        line_highlights = util.make_bg_highlights(bgcolor, "#CC3300", 10),
+        vt_highlight = util.make_fg_highlight("#CC3300"),
+      })
+    end
+  },
+
 }

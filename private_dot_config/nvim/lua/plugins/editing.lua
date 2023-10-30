b42564681fb019f493b2dacd575e4a4e003daa74
Juan Barajas <jbperches@gmail.com>
2023-10-29 22:44:47 -0600

    feat(nvim): remove space on some surrounds

--- private_dot_config/nvim/lua/plugins/editing.lua
+++ private_dot_config/nvim/lua/plugins/editing.lua
@@ -308,6 +308,13 @@ return {
     "echasnovski/mini.surround",
     config = function ()
       require('mini.surround').setup({
+        custom_surroundings = {
+          -- don't insert space on opening brackets
+          ["("] = { output = { left = "(", right = ")" } },
+          ["["] = { output = { left = "[", right = "]" } },
+          ["{"] = { output = { left = "{", right = "}" } },
+          ["<"] = { output = { left = "<", right = ">" } },
+        },
         search_method = "cover_or_next",
         mappings = {
           add = 'ys',
@@ -317,18 +324,13 @@ return {
           highlight = '',
           replace = 'cs',
           update_n_lines = '',
-
-          -- Add this only if you don't want to use extended mappings
           suffix_last = '',
           suffix_next = '',
         },
       })
 
-      -- Remap adding surrounding to Visual mode selection
       vim.api.nvim_del_keymap('x', 'ys')
       vim.api.nvim_set_keymap('x', 'S', [[:<C-u>lua MiniSurround.add('visual')<CR>]], { noremap = true })
-
-      -- Make special mapping for "add surrounding for line"
       vim.api.nvim_set_keymap('n', 'yss', 'ys_', { noremap = false })
     end,
   },

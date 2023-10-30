3903fe5f65a15cedeae7b8604cf87327bff50266
Juan Barajas <jbperches@gmail.com>
2023-10-29 22:43:03 -0600

    feat(nvim): use ultimate-autopairs

--- private_dot_config/nvim/lua/plugins/editing.lua
+++ private_dot_config/nvim/lua/plugins/editing.lua
@@ -64,26 +64,14 @@ return {
     end
   },
 
-  -- Autopairs
   {
-    "windwp/nvim-autopairs",
+    "altermo/ultimate-autopair.nvim",
     event = "InsertEnter",
-    config = function()
-      local autopairs = require("nvim-autopairs")
-      local autopairs_rule = require("nvim-autopairs.rule")
-      local autopairs_cond = require('nvim-autopairs.conds')
-
-      autopairs.setup({})
-
-      -- note: careful, order of :with_pair seems to matter
-      autopairs.add_rules({
-        autopairs_rule("__", "__", "python"),
-
-        autopairs_rule(' ', ' ', 'lua')
-          :with_pair(autopairs_cond.not_after_regex_check("[^%}]", 1))
-          :with_pair(autopairs_cond.before_text_check("{"))
+    config = function ()
+      require('ultimate-autopair').setup({
+        { '__', '__', cmap=false, newline=false, space=true, ft={ "python" } },
       })
-    end
+    end,
   },
 
   -- Autocomplete

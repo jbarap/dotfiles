a64897f2ff9a4d05e7c5f38f147eb0d4f3e6779e
Juan Barajas <jbperches@gmail.com>
2024-08-18 12:45:37 -0600

    feat(nvim): add whole buffer text object

--- private_dot_config/nvim/lua/plugins/editing.lua
+++ private_dot_config/nvim/lua/plugins/editing.lua
@@ -386,6 +386,16 @@ return {
       require("mini.ai").setup({
         n_lines = 500,
         search_method = 'cover_or_nearest',
+        custom_textobjects = {
+          g = function ()
+            local from = { line = 1, col = 1 }
+            local to = {
+              line = vim.fn.line("$"),
+              col = math.max(vim.fn.getline("$"):len(), 1)
+            }
+            return { from = from, to = to }
+          end
+        },
       })
     end,
   },

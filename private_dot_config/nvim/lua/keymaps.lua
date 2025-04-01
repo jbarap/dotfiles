25d063fc8299898dcb527738039c279e3d3bdec9
Juan Barajas <jbperches@gmail.com>
2025-03-31 18:11:38 -0600

    feat(nvim): use virtual lines for diagnostic jumps

--- private_dot_config/nvim/lua/keymaps.lua
+++ private_dot_config/nvim/lua/keymaps.lua
@@ -60,11 +60,34 @@ set_keymap("n", "gK",  function()
   vim.diagnostic.config({ virtual_lines = virt_lines })
   end, { desc = "Toggle diagnostics in virtual lines" }
 )
+
+local function jump_with_virt_line(jump_count)
+  -- prevent autocmd for repeated jumps
+  pcall(vim.api.nvim_del_augroup_by_name, "jumpWithVirtLineDiags")
+
+  vim.diagnostic.jump({ count = jump_count })
+  vim.diagnostic.config({ virtual_lines = { current_line = true } })
+
+  -- deferred to not be triggered by the jump itself
+  vim.defer_fn(function()
+    vim.api.nvim_create_autocmd("CursorMoved", {
+      desc = "User(once): Reset diagnostics virtual lines.",
+      once = true,
+      group = vim.api.nvim_create_augroup("jumpWithVirtLineDiags", {}),
+      callback = function()
+        vim.diagnostic.config({ virtual_lines = false })
+      end,
+    })
+  end, 1)
+end
+
 set_keymap("n", "]d", function()
-  vim.diagnostic.jump({count = 1, float = true})
+  -- vim.diagnostic.jump({count = 1, float = true})
+  jump_with_virt_line(1)
 end, { desc = "Next diagnostic" })
 set_keymap("n", "[d", function()
-  vim.diagnostic.jump({count = -1, float = true})
+  -- vim.diagnostic.jump({count = -1, float = true})
+  jump_with_virt_line(-1)
 end, { desc = "Prev diagnostic" })
 
 

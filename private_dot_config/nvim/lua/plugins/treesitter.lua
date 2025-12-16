d59c503265285df0df4d0843120d0a328ebd3173
Juan Barajas <jbperches@gmail.com>
2025-12-15 19:00:11 -0600

    fix: switch treesitter to main

--- private_dot_config/nvim/lua/plugins/treesitter.lua
+++ private_dot_config/nvim/lua/plugins/treesitter.lua
@@ -1,146 +1,196 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    version = false,
+    lazy = false,
+    branch = "main",
     build = ":TSUpdate",
-    event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-      {
-        "yioneko/nvim-yati",
-        event = { "BufReadPost", "BufNewFile" },
-      },
-      { "nvim-treesitter/nvim-treesitter-textobjects" },
-    },
-    keys = {
-      { "<CR>", desc = "Increment selection" },
-      { "<S-CR>", desc = "Schrink selection", mode = "x" },
-    },
     config = function()
-      local disabled_buffers = {}
+      local ts = require("nvim-treesitter")
 
-      local function disable_on_big_file(lang, bufnr)
-        local stat = vim.loop.fs_stat(vim.api.nvim_buf_get_name(bufnr))
-        if stat == nil then
-          return false
-        end
+      local parsers = {
+        "bash",
+        -- "comment",
+        "css",
+        "diff",
+        "dockerfile",
+        "git_config",
+        "git_rebase",
+        "gitcommit",
+        "gitignore",
+        "go",
+        "hcl",
+        "html",
+        "http",
+        "java",
+        "javascript",
+        "jsdoc",
+        "json",
+        "json5",
+        "jsonc",
+        "latex",
+        "lua",
+        "luadoc",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "regex",
+        "rst",
+        "rust",
+        "scss",
+        "ssh_config",
+        "sql",
+        "terraform",
+        "typst",
+        "toml",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "xml",
+        "yaml",
+      }
+      ts.install(parsers)
 
-        local is_big = stat.size > 5000000 -- disable on files > 5MB
+      local group = vim.api.nvim_create_augroup("TreesitterSetup", { clear = true })
 
-        -- Hardcoding this to false while I test performance on big files
-        is_big = false
+      local ignore_filetypes = {
+        "checkhealth",
+        "lazy",
+        "mason",
+        "snacks_dashboard",
+        "snacks_notif",
+        "snacks_win",
+      }
 
-        if is_big and disabled_buffers[bufnr] == nil then
-          vim.notify("File is greater than 5MB, disabling treesitter for improved performance.", vim.log.levels.INFO)
-          disabled_buffers[bufnr] = true
-        end
+      -- For indenting see: https://github.com/nvim-treesitter/nvim-treesitter/issues/7840
+      vim.api.nvim_create_autocmd("FileType", {
+        group = group,
+        desc = "Enable treesitter highlighting",
+        callback = function(event)
+          local buf = event.buf
+          local filetype = event.match
 
-        return is_big
-      end
+          if vim.tbl_contains(ignore_filetypes, filetype) then
+            return
+          end
 
-      local function disable_for_lang(lang, black_list)
-        return vim.tbl_contains(black_list, lang)
-      end
+          local lang = vim.treesitter.language.get_lang(filetype) or filetype
 
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = "all",
-        ignore_install = { "comment" },
+          -- Don't run on languages without parsers
+          if not vim.treesitter.language.add(lang) then
+              return
+          end
 
-        -- highlight performance is slow on big files
-        highlight = {
-          enable = true,
-          disable = function (lang, bufnr)
-            return disable_on_big_file(lang, bufnr)
-              or disable_for_lang(lang, { "dockerfile" })
-          end,
-        },
-
-        playground = {
-          enable = true,
-          disable = disable_on_big_file,
-        },
-
-        incremental_selection = {
-          enable = true,
-          disable = disable_on_big_file,
-          keymaps = {
-            init_selection = "<CR>",
-            scope_incremental = "<CR>",
-            node_incremental = "<TAB>",
-            node_decremental = "<S-TAB>",
-          },
-        },
-
-        -- indenting with TS is really slow for some reason
-        indent = {
-          enable = false,
-        },
-
-        yati = {
-          enable = true,
-          disable = disable_on_big_file,
-          default_lazy = true,
-          default_fallback = "auto",
-        },
-
-        textobjects = {
-          select = {
-            enable = true,
-            disable = disable_on_big_file,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-              ["aa"] = "@parameter.outer",
-              ["ia"] = "@parameter.inner",
-              ["aF"] = "@call.outer",
-              ["iF"] = "@call.inner",
-              ["ab"] = "@block.outer",
-              ["ib"] = "@block.inner",
-            },
-            selection_modes = {
-              ["@function.outer"] = "V",
-              ["@class.outer"] = "V",
-            },
-          },
-          move = {
-            enable = true,
-            disable = disable_on_big_file,
-            set_jumps = false, -- whether to set jumps in the jumplist
-            goto_next_start = {
-              ["]f"] = "@function.outer",
-              ["]c"] = "@class.outer",
-              ["]b"] = "@block.outer",
-              ["]a"] = "@parameter.inner",
-            },
-            goto_next_end = {
-              ["]F"] = "@function.outer",
-              ["]C"] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[f"] = "@function.outer",
-              ["[c"] = "@class.outer",
-              ["[b"] = "@block.outer",
-              ["[a"] = "@parameter.inner",
-            },
-            goto_previous_end = {
-              ["[F"] = "@function.outer",
-              ["[C"] = "@class.outer",
-            },
-          },
-
-          lsp_interop = {
-            enable = true,
-            disable = disable_on_big_file,
-            border = "rounded",
-            peek_definition_code = {
-              ["<leader>pd"] = "@function.outer",
-            },
-          },
-        },
-
+          -- Start highlighting immediately (works if parser exists)
+          pcall(vim.treesitter.start, buf, lang)
+        end,
       })
     end
   },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    init = function()
+      -- Disable entire built-in ftplugin mappings to avoid conflicts.
+      -- See https://github.com/neovim/neovim/tree/master/runtime/ftplugin for built-in ftplugins.
+      vim.g.no_plugin_maps = true
+    end,
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        select = {
+          lookahead = true,
+          selection_modes = {
+            ["@parameter.outer"] = "v", -- charwise
+            ["@function.outer"] = "V", -- linewise
+            ["@class.outer"] = "<c-v>", -- blockwise
+          },
+          include_surrounding_whitespace = false,
+        },
+        move = {
+          set_jumps = false,
+        },
+      })
+
+      -- Select
+      vim.keymap.set({ "x", "o" }, "af", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "if", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@function.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ac", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@class.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ic", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@class.inner", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "aa", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@parameter.outer", "textobjects")
+      end)
+      vim.keymap.set({ "x", "o" }, "ia", function()
+        require "nvim-treesitter-textobjects.select".select_textobject("@parameter.inner", "textobjects")
+      end)
+
+      -- Move
+      -- next start
+      vim.keymap.set({ "n", "x", "o" }, "]f", function()
+        require("nvim-treesitter-textobjects.move").goto_next_start("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "]c", function()
+        require("nvim-treesitter-textobjects.move").goto_next_start("@class.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "]a", function()
+        require("nvim-treesitter-textobjects.move").goto_next_start("@parameter.inner", "textobjects")
+      end)
+
+      -- next end
+      vim.keymap.set({ "n", "x", "o" }, "]F", function()
+        require("nvim-treesitter-textobjects.move").goto_next_end("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "]C", function()
+        require("nvim-treesitter-textobjects.move").goto_next_end("@class.outer", "textobjects")
+      end)
+
+      -- prev start
+      vim.keymap.set({ "n", "x", "o" }, "[f", function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "[c", function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start("@class.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "[a", function()
+        require("nvim-treesitter-textobjects.move").goto_previous_start("@parameter.inner", "textobjects")
+      end)
+
+      -- prev end
+      vim.keymap.set({ "n", "x", "o" }, "[F", function()
+        require("nvim-treesitter-textobjects.move").goto_previous_end("@function.outer", "textobjects")
+      end)
+      vim.keymap.set({ "n", "x", "o" }, "[C", function()
+        require("nvim-treesitter-textobjects.move").goto_previous_end("@class.outer", "textobjects")
+      end)
+
+    end,
+  },
+  {
+    -- while more LSPs support textDocument/selectionRange
+    -- see :h vim.lsp.buf.selection_range
+    -- https://github.com/neovim/neovim/pull/34011/files
+    "MeanderingProgrammer/treesitter-modules.nvim",
+    dependencies = { "nvim-treesitter/nvim-treesitter" },
+    event = "VeryLazy",
+    opts = {
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<CR>",
+          scope_incremental = "<CR>",
+          node_incremental = "<TAB>",
+          node_decremental = "<S-TAB>",
+        },
+      },
+    },
+  },
+
 }
